5020e1eed1540b7e6210c78afc00efe97491ad1f
JaDogg <JaDogg@users.noreply.github.com>
2022-09-15 09:25:43 +0100

    test(e2e): added carpntr based testing support for e2e

--- scripts/e2e.py
+++ scripts/e2e.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import difflib
 import json
 import os
 import re
@@ -13,7 +16,7 @@ SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 LIBS_DIR = os.path.abspath(os.path.join(os.path.dirname(SCRIPT_DIR), "libs"))
 CARPNTR = os.path.abspath(os.path.join(os.path.dirname(SCRIPT_DIR), "carpntr", "build", "carpntr"))
 MAX_EXECUTION_TIME_SEC = 30
-OUTPUT = os.path.join(os.path.dirname(SCRIPT_DIR), "comp_output_test", "e2e.json")
+OUTPUT = os.path.join(SCRIPT_DIR, "e2e_snapshot.json")
 E2E_FILES = os.path.join(SCRIPT_DIR, "e2e_files")
 
 
@@ -76,17 +79,54 @@ def get_cleaned_output(sout: str, serr: str) -> str:
     return "\n".join(output_lines).strip()
 
 
-def main() -> int:
+def generic_tokenize(s: str) -> List[str]:
+    result = s.splitlines(keepends=False)
+    result = "\n".join(result)
+    return result.splitlines(keepends=True)
+
+
+def colour_diff(s: str) -> str:
+    if s.startswith("---") or s.startswith("+++"):
+        return Colors.blue(s)
+    if s.startswith("+"):
+        return Colors.green(s)
+    if s.startswith("-"):
+        return Colors.fail(s)  # fail is red
+    return s
+
+
+def perform_test(current: str, snapshot: str, path: str) -> bool:
+    ct = generic_tokenize(current)
+    snapt = generic_tokenize(snapshot)
+    any_mismatches = False
+    if len(ct) == len(snapt):
+        for c, s in zip(ct, snapt):
+            if c != s:
+                any_mismatches = True
+                break
+    else:
+        any_mismatches = True
+    print(Colors.blue("Test:"), Colors.cyan(path))
+    if any_mismatches:
+        print("-", Colors.fail("failed"))
+        difference = difflib.unified_diff(snapt, ct, fromfile='snapshot', tofile='current', lineterm="\n")
+        difference = "".join([" " + colour_diff(str(x)) for x in difference])
+        print(difference)
+    else:
+        print("-", Colors.green("passed"))
+    return not any_mismatches
+
+
+def main(test_mode: bool) -> int:
     print(Colors.cyan("""    
 ███████ ██████  ███████ 
 ██           ██ ██      
 █████    █████  █████   
 ██      ██      ██      
 ███████ ███████ ███████ 
------- Testing --------
--- Snapshot Generator -
-"""))
+------ Tester ---------"""))
     results = {}
+    print(Colors.warning("Generating new snapshot..."))
     with open(E2E_FILES, "r", encoding="utf=8") as h:
         for path in h:
             full_path = os.path.abspath(os.path.join(os.path.dirname(SCRIPT_DIR), path.strip()))
@@ -98,15 +138,10 @@ def main() -> int:
             preserve_win_bin = os.path.exists(filename_base + ".exe")
             preserve_c_out = os.path.exists(filename_base + ".c")
             commandline = [CARPNTR, "-RS", filename]
-            print(Colors.warning("Executing:"))
-            print(Colors.blue("- dir:"), Colors.cyan(parent))
-            print(Colors.blue("- cmd:"), Colors.cyan(commandline))
+            print(Colors.warning("Executing:"), path.strip())
             so, se, ret = execute(commandline)
             output = get_cleaned_output(so, se)
-            print(Colors.fail("-" * 40))
-            print(output)
-            print(Colors.fail("-" * 40))
-            results[path.strip()] = {"o": output, "ret": ret}
+            results[path.strip()] = output + "\n\nreturn := " + str(ret)
             if not preserve_unix_bin:
                 try:
                     os.unlink(filename_base)
@@ -122,10 +157,56 @@ def main() -> int:
                     os.unlink(filename_base + ".c")
                 except OSError:
                     pass
+    print(Colors.blue("Done."))
+
+    print(Colors.warning("Testing..."))
+    success = True
+    if test_mode:
+        try:
+            with open(OUTPUT, "r+", encoding="utf-8") as h:
+                original_snapshot = json.load(h)
+        except OSError:
+            original_snapshot = {}
+        passed = True
+
+        tests = sorted(list(results.keys()))
+        for k in tests:
+            v = results[k]
+            original = str(original_snapshot.get(k, ""))
+            try:
+                del original_snapshot[k]
+            except (ValueError, KeyError) as _:
+                pass
+            passed = perform_test(v, original, k) and passed
+
+        tests = sorted(list(original_snapshot.keys()))
+        for ok in tests:
+            ov = original_snapshot[ok]
+            current = ""
+            passed = perform_test(current, ov, ok) and passed
+        success = passed
+
+    if success:
+        print(Colors.warning("Testing:"), Colors.green("------> All passed"))
+    else:
+        print(Colors.warning("Testing:"), Colors.fail("------> Failed."))
+
+    print(Colors.warning("Writing new snapshot.."))
     with open(OUTPUT, "w+", encoding="utf-8") as h:
         h.write(json.dumps(results))
-    return EXIT_SUCCESS
+    print(Colors.blue("Done."))
+    return EXIT_SUCCESS if success else EXIT_FAILURE
 
 
 if __name__ == "__main__":
-    quit(main())
+    mode = None
+    print(sys.argv)
+    if len(sys.argv) == 1:
+        mode = "test"
+    if len(sys.argv) == 2:
+        mode = sys.argv[1]
+    if not mode or (mode != "test" and mode != "snapshot"):
+        print("Invalid mode")
+        print("Usage: python e2e.py test | python e2e.py snapshot")
+        quit(EXIT_FAILURE)
+    quit(main(mode == "test"))
