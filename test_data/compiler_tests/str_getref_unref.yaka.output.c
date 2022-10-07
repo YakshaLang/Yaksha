4316f2e47858d85da4198d81a85630cf84e79f0d
JaDoggx86 <JaDogg@users.noreply.github.com>
2022-10-07 21:43:34 +0100

    test(test_compiler): added a `pass` statement test

--- test_data/compiler_tests/str_getref_unref.yaka.output.c
+++ test_data/compiler_tests/str_getref_unref.yaka.output.c
@@ -26,6 +26,7 @@ void yy__world2(yk__sds* yy__a)
 }
 int32_t yy__main() 
 {
+    yk__sds yy__my_empty = yk__sdsempty();
     yk__sds t__2 = yk__sdsnew("hello");
     yk__sds yy__hello = yk__sdsdup(t__2);
     int32_t yy__i = INT32_C(5);
@@ -39,8 +40,11 @@ int32_t yy__main()
             yy__i = (yy__i - INT32_C(1));
         }
     }
+    yk__printlnstr(((yy__my_empty != NULL)) ? "True" : "False");
+    yy__world1((&(yy__my_empty)));
     yk__sdsfree(yy__hello);
     yk__sdsfree(t__2);
+    yk__sdsfree(yy__my_empty);
     return INT32_C(0);
 }
 #if defined(YK__MINIMAL_MAIN)
--- test_data/compiler_tests/voidfunc.yaka.output.c
+++ test_data/compiler_tests/voidfunc.yaka.output.c
@@ -2,11 +2,17 @@
 #include "yk__lib.h"
 // --forward declarations-- 
 void yy__sample();
+void yy__sample2();
 int32_t yy__main();
 // --structs-- 
 // --functions-- 
 void yy__sample() 
 {
+    return;
+}
+void yy__sample2() 
+{
+    // pass;
     return;
 }
 int32_t yy__main() 
