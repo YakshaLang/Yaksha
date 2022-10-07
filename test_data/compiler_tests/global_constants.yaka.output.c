9651e058da526ddca7ef3ae8b2c4aa9141a1e51e
JaDoggx86 <JaDogg@users.noreply.github.com>
2022-10-07 23:01:13 +0100

    test(test_compiler): added a local constant let test

--- test_data/compiler_tests/global_constants.yaka.output.c
+++ test_data/compiler_tests/global_constants.yaka.output.c
@@ -19,6 +19,7 @@ int32_t yy__main();
 // --functions-- 
 int32_t yy__main() 
 {
+    int32_t const  yy__local_const = INT32_C(4);
     yk__printlnuint(((uintmax_t)yy__constants_file_MY_CONST_VAL));
     return INT32_C(0);
 }
