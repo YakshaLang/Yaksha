b1f8d86d57e81731847d049b04b680be94841d95
JaDoggx86 <JaDogg@users.noreply.github.com>
2022-10-07 20:51:22 +0100

    test(test_compiler): added additional binary operator tests for float, f64 % and str !=

--- test_data/compiler_tests/operator_test.yaka.output.c
+++ test_data/compiler_tests/operator_test.yaka.output.c
@@ -7,6 +7,7 @@ void yy__print_b(float);
 int32_t yy__augment_equal_b();
 void yy__print_c(double);
 int32_t yy__augment_equal_c();
+int32_t yy__additional_operators();
 int32_t yy__main();
 // --structs-- 
 // --functions-- 
@@ -92,29 +93,55 @@ int32_t yy__augment_equal_c()
     yy__print_c(yy__c);
     return INT32_C(3);
 }
+int32_t yy__additional_operators() 
+{
+    yk__sds t__3 = yk__sdsnew("Hello");
+    yk__sds yy__a = yk__sdsdup(t__3);
+    yk__sds t__4 = yk__sdsnew("World");
+    yk__sds yy__b = yk__sdsdup(t__4);
+    bool yy__c = (yk__sdscmp(yy__a , yy__b) != 0);
+    float yy__d = 5.0f;
+    float yy__e = 4.0f;
+    double yy__f = 10.0;
+    double yy__g = 20.0;
+    if (yy__c)
+    {
+        yy__e = remainderf(yy__d, yy__e);
+    }
+    else
+    {
+        yy__g = remainder(yy__f, yy__g);
+    }
+    yk__sdsfree(yy__b);
+    yk__sdsfree(t__4);
+    yk__sdsfree(yy__a);
+    yk__sdsfree(t__3);
+    return INT32_C(0);
+}
 int32_t yy__main() 
 {
     int32_t yy__v = INT32_C(0);
     yy__v += yy__augment_equal_a();
     yy__v += yy__augment_equal_b();
     yy__v += yy__augment_equal_c();
-    yk__sds t__3 = yk__sdsnew("v=");
-    yk__printstr((t__3));
+    yy__v += yy__additional_operators();
+    yk__sds t__5 = yk__sdsnew("v=");
+    yk__printstr((t__5));
     yk__printlnint(((intmax_t)yy__v));
-    yk__sds t__4 = yk__sdsnew("r=");
-    yk__printstr((t__4));
+    yk__sds t__6 = yk__sdsnew("r=");
+    yk__printstr((t__6));
     int32_t yy__r = (((INT32_C(1) << INT32_C(2)) ^ (INT32_C(0xDEADBEEF) & INT32_C(0xCAFE))) | (INT32_C(0xBEE0) >> INT32_C(1)));
     yk__printlnint(((intmax_t)yy__r));
-    yk__sds t__5 = yk__sdsnew("Hello");
-    yk__sds yy__s = yk__sdsdup(t__5);
-    yk__sds t__6 = yk__sdsnew(" World");
-    yy__s = yk__sdscatsds(yy__s, t__6);
+    yk__sds t__7 = yk__sdsnew("Hello");
+    yk__sds yy__s = yk__sdsdup(t__7);
+    yk__sds t__8 = yk__sdsnew(" World");
+    yy__s = yk__sdscatsds(yy__s, t__8);
     yk__printlnstr((yy__s));
-    yk__sdsfree(t__6);
+    yk__sdsfree(t__8);
     yk__sdsfree(yy__s);
+    yk__sdsfree(t__7);
+    yk__sdsfree(t__6);
     yk__sdsfree(t__5);
-    yk__sdsfree(t__4);
-    yk__sdsfree(t__3);
     return INT32_C(0);
 }
 #if defined(YK__MINIMAL_MAIN)
