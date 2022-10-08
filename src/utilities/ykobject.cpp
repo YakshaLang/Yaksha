f65aa8b5951576953fc03d8721a984b4e4a81094
JaDoggx86 <JaDogg@users.noreply.github.com>
2022-10-08 22:38:32 +0100

    fix(ykobject): clean up non-needed functionality

--- src/utilities/ykobject.cpp
+++ src/utilities/ykobject.cpp
@@ -1,6 +1,5 @@
 // ykobject.cpp
 #include "utilities/ykobject.h"
-#include "utilities/error_printer.h"
 #include <sstream>
 #include <utility>
 using namespace yaksha;
@@ -28,30 +27,12 @@ ykobject::ykobject(bool i, ykdt_pool *pool)
     : object_type_(object_type::PRIMITIVE_OR_OBJ), bool_val_(i) {
   datatype_ = pool->create("bool");
 }
-ykobject::ykobject(const std::string &str, token *bad_token)
-    : flow_(control_flow_change::ERROR) {
-  std::stringstream error_message{};
-  error_message << "\n";
-  errors::print_token(error_message, *bad_token);
-  error_message << " --> " << str;
-  string_val_ = error_message.str();
-  object_type_ = object_type::RUNTIME_ERROR;
-}
-ykobject::ykobject(control_flow_change flow_change)
-    : object_type_{object_type::RUNTIME_ERROR}, flow_{flow_change},
-      string_val_("break/continue") {}
 bool ykobject::is_primitive_or_obj() const {
   return object_type_ == object_type::PRIMITIVE_OR_OBJ;
 }
 bool ykobject::is_a_function() const {
   return object_type_ == object_type::FUNCTION ||
          object_type_ == object_type::MODULE_FUNCTION;
-}
-bool ykobject::is_same_datatype(ykobject &other) const {
-  if (is_primitive_or_obj() && other.is_primitive_or_obj()) {
-    return *datatype_ == *other.datatype_;
-  }
-  return object_type_ == other.object_type_;
 }
 ykobject::ykobject(ykdatatype *dt) {
   datatype_ = dt;
--- src/utilities/ykobject.h
+++ src/utilities/ykobject.h
@@ -7,7 +7,6 @@
 #include "ykdt_pool.h"
 #include <string>
 namespace yaksha {
-  enum class control_flow_change { BREAK, RETURN, CONTINUE, NO_CHANGE, ERROR };
   enum class object_type {
     PRIMITIVE_OR_OBJ,
     FUNCTION,
@@ -25,49 +24,19 @@ namespace yaksha {
     explicit ykobject(int i, ykdt_pool *pool);
     explicit ykobject(bool b, ykdt_pool *pool);
     explicit ykobject(std::string str, ykdt_pool *pool);
-    explicit ykobject(const std::string &str, token *bad_token);
     explicit ykobject(double dbl, ykdt_pool *pool);
     explicit ykobject(float dbl, ykdt_pool *pool);
-    explicit ykobject(control_flow_change flow_change);
     explicit ykobject(ykdt_pool *pool);
     [[nodiscard]] bool is_primitive_or_obj() const;
     [[nodiscard]] bool is_a_function() const;
-    bool is_same_datatype(ykobject &other) const;
     int integer_val_{0};
     std::string string_val_{};
     std::string module_file_{};
     std::string module_name_{};
     double double_val_{};
     bool bool_val_{};
-    control_flow_change flow_ = control_flow_change::NO_CHANGE;
     object_type object_type_{object_type::PRIMITIVE_OR_OBJ};
     ykdatatype *datatype_{nullptr};
-    /**
-     * Write ykobject representation
-     * @param out output stream to write to
-     * @param s object
-     * @return output stream
-     */
-    inline friend std::ostream &operator<<(std::ostream &out,
-                                           const ykobject &s) {
-      if (s.object_type_ == object_type::RUNTIME_ERROR) {
-        out << "[Error]::" << s.string_val_;
-      } else if (s.object_type_ == object_type::PRIMITIVE_OR_OBJ) {
-        if (s.datatype_->is_str()) {
-          out << string_utils::repr_string(s.string_val_);
-        } else if (s.datatype_->is_f64()) {
-          out << s.double_val_;
-        } else if (s.datatype_->is_i32()) {
-          out << s.integer_val_;
-        } else if (s.datatype_->is_none()) {
-          out << "None";
-        } else if (s.datatype_->is_bool()) {
-          out << (s.bool_val_ ? "True" : "False");
-        }
-      }
-      return out;
-    }
-    std::ostream &stringify(std::ostream &out) const { return out << *this; }
   };
 }// namespace yaksha
 #endif
