0d5a76cf7174eaa48848a05aa3035350ce6e9a21
JaDogg <JaDogg@users.noreply.github.com>
2022-10-23 04:05:55 +0100

    fix(runtime.rayextra): fixes to get/set game state functions

--- runtime/raylib_additional/rayextras.h
+++ runtime/raylib_additional/rayextras.h
@@ -1,14 +1,14 @@
 // ----- This file is to be directly included at the top of raylib game ---
 void *yk__raylib_game_state = (void *) 0;
-#if defined(YK__CR_DLL)
+void yy__game_step(void *);
 void *yk__get_game_state() { return yk__raylib_game_state; }
 void yk__set_game_state(void *gs) { yk__raylib_game_state = gs; }
+#if defined(YK__CR_DLL)
 #define yk__rungameloop(nn__fps, nn__state)                                    \
   do {                                                                         \
     yk__raylib_game_state = nn__state;                                         \
     SetTargetFPS(nn__fps);                                                     \
   } while (0)
-void yy__game_step(void *);
 #elif defined(PLATFORM_WEB)
 #include <emscripten/emscripten.h>
 #define yk__rungameloop(nn__fps, nn__state)                                    \
@@ -16,7 +16,6 @@ void yy__game_step(void *);
     yk__raylib_game_state = nn__state;                                         \
     emscripten_set_main_loop(yk__game_loop_wrapper, nn__fps, 1);               \
   } while (0)
-void yy__game_step(void *);
 void yk__game_loop_wrapper() { yy__game_step(yk__raylib_game_state); }
 #else
 #define yk__rungameloop(nn__fps, nn__state)                                    \
