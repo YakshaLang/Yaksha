be824a9810cd74c469a4272a209224242cf9717e
JaDogg <JaDogg@users.noreply.github.com>
2024-03-04 22:04:43 +0000

    ci: bump to 0.0.7

--- compiler/carpntr/main.yaka.c
+++ compiler/carpntr/main.yaka.c
@@ -3733,7 +3733,7 @@ yk__sds yy__os_which(yk__sds yy__os_binary)
 }
 yk__sds yy__libs_version() 
 {
-    return yk__sdsnewlen("0.0.6", 5);
+    return yk__sdsnewlen("0.0.7", 5);
 }
 void yy__print_banner() 
 {
