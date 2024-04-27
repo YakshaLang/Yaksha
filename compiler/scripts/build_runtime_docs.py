b8c6449de3b0ded4c2963be5055bf91acbcf2416
JaDogg <JaDogg@users.noreply.github.com>
2024-04-27 21:52:34 +0100

    feat(build_runtime_docs.py): add ability to export runtime docs as a json

--- compiler/scripts/build_runtime_docs.py
+++ compiler/scripts/build_runtime_docs.py
@@ -36,17 +36,97 @@
 # to the licenses of linked runtime libraries (see compiler/runtime/README.md).
 #
 # ==============================================================================================
+import json
 import os
+import xml.etree.ElementTree as ElementTree
 
 RUNTIME_PATH = os.path.join(os.path.dirname(__file__), "..", "runtime")
 RUNTIME_DOCS_DOCKERFILE = os.path.join(os.path.dirname(__file__), "..", "runtime_docs.Dockerfile")
 OUTPUT_PATH = os.path.join(os.path.dirname(__file__), "..", "comp_output_test")
+
+def cleanup_json_data(json_data):
+    children = json_data.get('children', [])
+    temp_dictionary = {}
+    temp_set = set()
+    result = []
+    for child in children:
+        if 'name' not in child:
+            result.append(child)
+            continue
+        if child['type'] == 'typedef':
+            child['datatype'] = child['datatype'][0]
+        if child['type'] == 'variable' and child['datatype'] == 'struct':
+            continue
+        if child['type'] == 'typedef' and 'datatype' in child and child['datatype'] == 's':
+            continue
+        if child['name'] in temp_dictionary:
+            already_discovered = temp_dictionary[child['name']]
+            serialized_child = json.dumps(child, sort_keys=True)
+            already_discovered_serialized = json.dumps(already_discovered, sort_keys=True)
+            if serialized_child == already_discovered_serialized:
+                continue
+            if serialized_child in temp_set:
+                continue
+            temp_set.add(serialized_child)
+        temp_dictionary[child['name']] = child
+        result.append(child)
+    json_data['children'] = result
+    return json_data
+
+def xml_to_json(xml_string):
+    root = ElementTree.fromstring(xml_string)
+    namespace = '{https://www.msweet.org}'
+
+    def parse_element(element):
+        result = {}
+        for child in element:
+            if child.tag in [f'{namespace}struct', f'{namespace}typedef', f'{namespace}variable',
+                             f'{namespace}function', f'{namespace}enumeration',
+                             f"{namespace}constant",
+                             f"{namespace}union", f"{namespace}argument",
+                             f"{namespace}returnvalue"
+                             ]:
+                if 'children' not in result:
+                    result['children'] = []
+                child_result = {'type': child.tag.replace(namespace, "")}
+                if 'name' in child.attrib:
+                    child_result['name'] = child.attrib['name']
+                content = child.text.strip() if child.text else ''
+                if content:
+                    child_result['content'] = content
+                child_result.update(parse_element(child))
+                result['children'].append(child_result)
+            elif child.tag in [f"{namespace}type", f"{namespace}description"]:
+                key = child.tag.replace(namespace, "")
+                if key == "type":
+                    key = "datatype"
+                if 'name' in child.attrib:
+                    result['name'] = child.attrib['name']
+                content = child.text.strip() if child.text else ''
+                if key in result:
+                    if isinstance(result[key], list):
+                        result[key].append(content)
+                    else:
+                        result[key] = [result[key], content]
+                else:
+                    result[key] = content
+            else:
+                print(f"Unknown tag: {child.tag}")
+        return result
+
+    json_data = cleanup_json_data(parse_element(root))
+    return json.dumps(json_data, indent=2)
 
 
 def main():
     os.chdir(os.path.dirname(os.path.dirname(__file__)))
     os.system(f"docker build -t yaksha-runtime-docs -f {RUNTIME_DOCS_DOCKERFILE} .")
     os.system(f"docker run -v {RUNTIME_PATH}:/runtime -v {OUTPUT_PATH}:/output yaksha-runtime-docs")
+    with open(os.path.join(OUTPUT_PATH, "runtime.xml")) as f:
+        xml_string = f.read()
+    json_string = xml_to_json(xml_string)
+    with open(os.path.join(OUTPUT_PATH, "runtime.json"), "w") as f:
+        f.write(json_string)
 
 
 if __name__ == "__main__":
